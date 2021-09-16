a00d23a79ad82ac69ec86027439e61796c7a372f
nordh <nordh@DESKTOP-60TM03E.home>
2021-09-15 23:13:25 -0400

    Third Commit

--- Selenium/src/test/java/TestScripts/TestClass.java
+++ Selenium/src/test/java/TestScripts/TestClass.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,10 +12,10 @@ import org.testng.annotations.Test;
 public class TestClass {
 	
 	WebDriver driver;
-	
+	String Title,Url;
 	public String url = "https://www.google.com/";
 	
-	@Test
+	@Test(priority=1)
 	public void test() {
 	System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\selenium-chrome-driver\\3.141.59\\chromedriver.exe");
 	driver = new ChromeDriver();
@@ -26,7 +27,23 @@ public class TestClass {
 	
 	String Title = driver.getTitle();
 	
-	System.out.println("asa"+Title);
+	System.out.println(Title);
+	}
+	
+	@Test(priority=2)
+	public void AssertTitle() {
+		
+		Title = driver.getTitle();
+		System.out.println("Method2"+Title);
+		Assert.assertTrue(Title.equalsIgnoreCase("Google"));
+	}
+
+	@Test(priority=3)
+	public void AssertURL() {
+		
+		Url = driver.getCurrentUrl();
+		System.out.println("Method3"+Url);
+		Assert.assertTrue(Url.contentEquals("https://www.google.com/"));
 	}
 	
 	@BeforeTest
