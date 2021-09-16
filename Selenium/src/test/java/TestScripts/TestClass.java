35bd01d757f8b914eb41ced06ec96be50b156b9a
nordh <nordh@DESKTOP-60TM03E.home>
2021-09-15 21:05:39 -0400

    First Commit

---
+++ Selenium/src/test/java/TestScripts/TestClass.java
@@ -0,0 +1,46 @@
+package TestScripts;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+	WebDriver driver;
+	
+	public String url = "https://www.google.com/";
+	
+	@Test
+	public void test() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\selenium-chrome-driver\\3.141.59\\chromedriver.exe");
+	driver = new ChromeDriver();
+	
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.manage().window().maximize();
+	
+	driver.get(url);
+	
+	String Title = driver.getTitle();
+	
+	System.out.println(Title);
+	}
+	
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("****Before Test****");
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		System.out.println("****DriverTerminated****");
+		driver.quit();
+		
+	}
+
+	
+
+}
